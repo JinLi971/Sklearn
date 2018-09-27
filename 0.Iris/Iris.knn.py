fec164138eaceaabb61f5ba7fff5fc8c128633f2
Jing Liu <jing.liu@it.uu.se>
2018-09-27 13:15:18 +0200

    find best K

--- 0.Iris/Iris.knn.py
+++ 0.Iris/Iris.knn.py
@@ -12,13 +12,15 @@ iris_y = iris.target
 print "data size:", iris_X.shape, iris_y.shape
 #print iris_y
 # Note that flowers are in order
-
 # permute data to generate trainning and testing dataset
 
+
+
+#test for once
 np.random.seed()
 indices = np.random.permutation(len(iris_X))
 
-number_testing = 10
+number_testing = 20
 
 #let the last number_tesing data to be testing dataset, and the rest to
 #be the trainning dataset
@@ -46,3 +48,30 @@ if (pred-iris_y_test).sum() == 0:
     print "Correct"
 else:
     print "Opps... Something went wrong in the prediction"
+
+
+#find the best K
+
+K_min = 3
+K_max = 20
+n_bootstrap = 100
+print "find the best K in (", K_min, ",", K_max, ") using the mean of 100 runs for each K"
+U = np.zeros(K_max - K_min)
+T = np.zeros(n_bootstrap)
+for n_neighbors in range(K_min,K_max):
+    for i in range(n_bootstrap):
+        np.random.seed()
+        indices = np.random.permutation(len(iris_X))
+        iris_X_train = iris_X[indices[:-number_testing]]
+        iris_y_train = iris_y[indices[:-number_testing]]
+        iris_X_test  = iris_X[indices[-number_testing:]]
+        iris_y_test  = iris_y[indices[-number_testing:]]
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(iris_X_train, iris_y_train)
+        pred = knn.predict(iris_X_test)
+        diff = pred-iris_y_test
+        #print diff
+        T[i] = np.count_nonzero(diff)
+    U[n_neighbors-K_min] = T.mean()
+
+print "error: ", U, "min error: ", U.min(), "min error at K = ", np.argmin(U)+K_min
